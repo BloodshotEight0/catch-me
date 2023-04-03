9566b75d05bc1eca3fb8c31d0b8882d7457cf280
BloodshotEight0 <brad20@hotmail.ca>
2023-04-03 18:25:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+input.onGesture(Gesture.LogoUp, function () {
+    dot.change(LedSpriteProperty.Y, 1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    dot.change(LedSpriteProperty.X, -1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    dot.change(LedSpriteProperty.X, 1)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    dot.change(LedSpriteProperty.Y, -1)
+})
+let dot2: game.LedSprite = null
+let dot: game.LedSprite = null
+dot = game.createSprite(2, 2)
 basic.forever(function () {
-	
+    dot2 = game.createSprite(randint(0, 4), randint(0, 4))
+    basic.pause(2000)
+    dot2.delete()
+})
+basic.forever(function () {
+    if (dot.isTouching(dot2)) {
+        game.addScore(1)
+    }
+})
+basic.forever(function () {
+    if (input.runningTime() > 10000) {
+        basic.showNumber(game.score())
+        game.gameOver()
+    }
 })
